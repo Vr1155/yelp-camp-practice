63d73537cfda6081e1adcfec20fbcb2f5a313df7
Vaibhav Ranashoor <vaibhav.ranshoor1155@gmail.com>
2022-10-22 15:38:28 +0530

    Created seed files which add exactly 50 random entires.

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,18 @@
-const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+// importing our seed dataset and helpers:
 
-// creating/connecting to a database called yelp-camp:
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+
+// Now importing mongoose and schema and setting up database connections:
+
+const mongoose = require("mongoose");
+// note that we are in seeds directory, so use ".."
+const Campground = require("../models/campground");
+
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-// useCreateIndex is no longer necessary in newer versions:
-// mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
@@ -20,8 +24,42 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+// this function contains the basic seed logic:
+
+// const seedDB = async () => {
+//   // delete everything that was there before:
+//   await Campground.deleteMany({});
+//   // create our new seed, for now just one 1 new instance:
+//   const c = new Campground({ title: "purple field" });
+//   // save it into our database:
+//   await c.save();
+// };
+
 const seedDB = async () => {
+  // delete everything that was there before:
   await Campground.deleteMany({});
+
+  const randomElemFromArr = Arr => Arr[Math.floor(Math.random() * Arr.length)];
+
+  // create our new seed using our dataset (we will make 50 such insertions):
+
+  for (let i = 0; i < 50; i++) {
+    // Math.random() returns any real num in interval [0,1).
+    const random1000 = Math.floor(Math.random() * 1000);
+    // picking a random city and state and generating a random title:
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${randomElemFromArr(descriptors)} ${randomElemFromArr(places)}`
+    });
+
+    await camp.save();
+  }
+
   const c = new Campground({ title: "purple field" });
+  // save it into our database:
   await c.save();
 };
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
