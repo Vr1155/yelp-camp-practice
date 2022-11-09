eeb8dd0f5f4b0b143fc98790b1d7ef124c346ef4
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-09 18:28:57 +0530

    added comments in farm and product files

--- models/farm.js
+++ models/farm.js
@@ -1,37 +1,36 @@
-const mongoose = require('mongoose');
-const Product = require('./product');
+const mongoose = require("mongoose");
+const Product = require("./product");
 const { Schema } = mongoose;
 
 const farmSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Farm must have a name!']
-    },
-    city: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: [true, 'Email required']
-    },
-    products: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Product'
-        }
-    ]
+  name: {
+    type: String,
+    required: [true, "Farm must have a name!"]
+  },
+  city: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: [true, "Email required"]
+  },
+  products: [
+    {
+      type: Schema.Types.ObjectId, // embedding objectid on both sides farm as well as product! (2 way referencing)
+      ref: "Product"
+    }
+  ]
 });
 
 // DELETE ALL ASSOCIATED PRODUCTS AFTER A FARM IS DELETED
-farmSchema.post('findOneAndDelete', async function (farm) {
-    if (farm.products.length) {
-        const res = await Product.deleteMany({ _id: { $in: farm.products } })
-        console.log(res);
-    }
-})
+farmSchema.post("findOneAndDelete", async function (farm) {
+  if (farm.products.length) {
+    const res = await Product.deleteMany({ _id: { $in: farm.products } });
+    console.log(res);
+  }
+});
 
-const Farm = mongoose.model('Farm', farmSchema);
-
-
-
+// What we're doing here is creating and exporting the Farm model.
+// This makes it so we can import the model into other parts of the program, like our routes
+const Farm = mongoose.model("Farm", farmSchema);
 module.exports = Farm;
--- models/product.js
+++ models/product.js
@@ -1,29 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-
 const productSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    category: {
-        type: String,
-        lowercase: true,
-        enum: ['fruit', 'vegetable', 'dairy']
-    },
-    farm: {
-        type: Schema.Types.ObjectId,
-        ref: 'Farm'
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  category: {
+    type: String,
+    lowercase: true,
+    enum: ["fruit", "vegetable", "dairy"]
+  },
+  farm: {
+    type: Schema.Types.ObjectId, // embedding objectid on both sides farm as well as product! (2 way referencing)
+    ref: "Farm"
+  }
 });
 
-
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product; 
+// What we're doing here is creating and exporting the Product model.
+// This makes it so we can import the model into other parts of the program, like our routes
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
