625f78c344d66187c9fee66e751a67433db3cd78
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-11 03:08:54 +0530

    implementing post middleware for deleting all reviews related to a particular campground when that particular campground is deleted.

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 // creating and exporting campground schema with mongoose:
 
@@ -16,6 +17,21 @@ const CampgroundSchema = new Schema({
       ref: "Review"
     }
   ]
+});
+
+// "findByIdAndDelete" calls "findOneAndDelete" which supports post middleware!
+// we can use this to delete all reviews of a particular camp whenever we delete that camp!
+
+CampgroundSchema.post("findOneAndDelete", async camp => {
+  if (camp) {
+    // if the camp exists, we delete all its reviews, for that we will use $in operato,
+    // The $in operator selects the documents or values,
+    // where the value of a field equals any value in the specified array.
+
+    // delete all reviews related to this campground.
+
+    await Review.deleteMany({ _id: { $in: camp.reviews } });
+  }
 });
 
 // exporting schema:
