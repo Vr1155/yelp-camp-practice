b1b836dd7e89cbb105b687ec7ccb85e8d5bed29c
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-16 17:28:02 +0530

    Using cookie-parser to read cookies on server.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+// cookie-parser was developed as another npm module,
+// probably because some projects dont need to parse cookies.
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+// using cookieParser as middleware to parse cookies:
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome to homePage!");
@@ -13,6 +19,16 @@ app.get("/", (req, res) => {
 app.get("/setName", (req, res) => {
   res.cookie("Name", "Doctor_wario");
   res.send("Your name has been set in cookies!");
+});
+
+app.get("/greet", (req, res) => {
+  // getting cookies:
+  const { Name = "No-Name" } = req.cookies;
+
+  console.log(`Welcome ${Name}`);
+  res.send(`Welcome ${Name}`);
+
+  // you can try it by changing the name in cookies in your browser's application tab and trying again!
 });
 
 app.listen(3000, () => {
