e3aa6b5be1f33b11a19eeeaaee2d33212e75d869
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-16 17:11:00 +0530

    Added route to set cookies

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express");
 
 const app = express();
-const shelterRoutes = require("./routes/shelters/shelters");
-const dogRoutes = require("./routes/dogs/dogs");
-const adminRoutes = require("./routes/admin/admin");
-
-// using our routers with a particular prefix path for each different routes!
-// using "/shelters" for shelter routes and "/dogs" for dogs route!
-app.use("/shelters", shelterRoutes);
-app.use("/dogs", dogRoutes);
-app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {
-  res.send("welcome to home page!!!!");
+  res.send("Welcome to homePage!");
+});
+
+app.get("/setName", (req, res) => {
+  res.cookie("Name", "Doctor_wario");
+  res.send("Your name has been set in cookies!");
 });
 
 app.listen(3000, () => {
