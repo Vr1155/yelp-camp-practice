b727d119455a3607ab73b7f0fc23e8d32ae95aa7
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-16 17:12:53 +0530

    Added some helpful comments

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Welcome to homePage!");
 });
 
+// cookies are used to store some information on the browser.
+// All information in cookies is also sent to server in each request.
+// They add some statefulness between requests.
+
 app.get("/setName", (req, res) => {
   res.cookie("Name", "Doctor_wario");
   res.send("Your name has been set in cookies!");
