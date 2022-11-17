f3fda37642b2b5d03ce595908475906c5c269191
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-17 16:34:27 +0530

    cleaning up for sessions practice

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
 const express = require("express");
-// cookie-parser was developed as another npm module,
-// probably because some projects dont need to parse cookies.
-const cookieParser = require("cookie-parser");
-
-const app = express();
-
-// using cookieParser as middleware to parse cookies:
-app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Welcome to homePage!");
-});
-
-// cookies are used to store some information on the browser.
-// All information in cookies is also sent to server in each request.
-// They add some statefulness between requests.
-
-app.get("/setName", (req, res) => {
-  res.cookie("Name", "Doctor_wario");
-  res.send("Your name has been set in cookies!");
-});
-
-app.get("/greet", (req, res) => {
-  // getting cookies:
-  const { Name = "No-Name" } = req.cookies;
-
-  console.log(`Welcome ${Name}`);
-  res.send(`Welcome ${Name}`);
-
-  // you can try it by changing the name in cookies in your browser's application tab and trying again!
-});
 
 app.listen(3000, () => {
   console.log("serving on port 3000");
