b6e14fdc75d58a10ebbacc33e9bc3b619c4ca9f9
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-27 01:26:24 +0530

    Refactored all campground related routes into their separate routes folder.

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 
+// importing models
 const Campground = require("./models/campground");
 const Review = require("./models/review");
 
+// importing routes:
+const campgroundRoutes = require("./routes/campground");
+
+// importing other dependencies:
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const ejsMate = require("ejs-mate");
@@ -63,45 +68,6 @@ app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 // you can also define your own format or build your own middleware (see different branch for middleware).
 
-// This function will do server side data validation using joi schema:
-const schemaValidatorJoi = (req, res, next) => {
-  // body contains a json object as a value which had a key of "campground"
-  // {"campground":{"title":"camp","location":"location"}}
-
-  // simple error throwing:
-  // if "campground" key is not present in the payload, throw an error:
-  // if (!req.body.campground)
-  //   throw new ExpressError("Invalid/Insufficent data", 400);
-
-  // as you can see this is tedious and quite difficult to scale.
-  // But here will use joi schema to make our job easier!
-
-  // validate() checks whether body is matching our Joi schema!
-  // result of validate(req.body) will be of following format:
-  // {
-  //   value: {
-  // original res.body
-  //          },
-  //   error: [Error [ValidationError]: "campground" is required]
-  //        {
-  //          _original: {},
-  //          details: [ [Object] ]
-  //        }
-  // }
-
-  const { error } = campgroundSchemaJoi.validate(req.body);
-
-  // notice that error.details is an array.
-
-  // if error exists, then throw expressError with all details joined by ",":
-  if (error) {
-    const msg = error.details.map(err => err.message).join(",");
-    next(new ExpressError(msg));
-  } else {
-    next();
-  }
-};
-
 const reviewSchmemaValidator = (req, res, next) => {
   const { error } = reviewSchemaJoi.validate(req.body);
   if (error) {
@@ -112,82 +78,18 @@ const reviewSchmemaValidator = (req, res, next) => {
   }
 };
 
+// Importing all Routes Here:
+
+// "/campgrounds" routes:
+app.use("/campgrounds", campgroundRoutes);
+
 // ALL GET REQUESTS:
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// async get request for displaying all campgrounds:
-app.get(
-  "/campgrounds",
-  asyncCatcher(async (req, res) => {
-    // finding all campgrounds will take time so you need await here:
-    const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", { campgrounds });
-  })
-);
-
-app.get("/campgrounds/new", (req, res) => {
-  const campground = {};
-  res.render("campgrounds/new", { campground });
-});
-
-// show details of individual campground using campground id:
-
-app.get(
-  "/campgrounds/:id",
-  asyncCatcher(async (req, res) => {
-    // finding campground with that specific id using findById(),
-    // it will always return 1 record since ids are unique:
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
-    // populate with reviews according to their objectid so we can show them on details page!
-    // notice that we have to write the name of the key which has value as array of objectids.
-    // in this case, "reviews" was the key which had stored the array of objectids which we want to populate with object values!
-    res.render("campgrounds/show", { campground });
-  })
-);
-
-// creates a dummy campground
-app.get(
-  "/makecampground",
-  asyncCatcher(async (req, res) => {
-    const camp = new Campground({
-      title: "My Backyard",
-      description: "Cheap Camping"
-    });
-    await camp.save();
-    res.send(camp);
-  })
-);
-
-app.get(
-  "/campgrounds/:id/edit",
-  asyncCatcher(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-
-    res.render("campgrounds/edit", { campground });
-  })
-);
-
 // All POST requests:
-
-// create new campground:
-
-app.post(
-  "/campgrounds",
-  schemaValidatorJoi, // does the server side data validations before running the post route
-  asyncCatcher(async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    // note that campground is now in a schema that we want, so we can call save on it:
-    await campground.save();
-    // now campground is a record in our database, we can access its id, so we can redirect:
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
 
 app.post(
   "/campgrounds/:id/reviews", // does server side validation for review post route
@@ -212,30 +114,7 @@ app.post(
   })
 );
 
-// All PUT requests:
-
-app.put(
-  "/campgrounds/:id/edit",
-  schemaValidatorJoi, // does the server side data validations before running the put route
-  asyncCatcher(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
-      ...req.body.campground
-    });
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
-
 // All DELETE requests:
-
-app.delete(
-  "/campgrounds/:id",
-  asyncCatcher(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-  })
-);
 
 app.delete(
   "/campgrounds/:id/reviews/:reviewId",
---
+++ routes/campground.js
@@ -0,0 +1,147 @@
+const express = require("express");
+
+// for routing:
+const router = express.Router();
+
+// Importing model:
+const Campground = require("../models/campground");
+
+// some other dependencies updated for this route:
+const { campgroundSchemaJoi } = require("../joiSchemas");
+const ExpressError = require("../utilities/ExpressError");
+const asyncCatcher = require("../utilities/asyncCatcher");
+
+// This function will do server side data validation using joi schema:
+const schemaValidatorJoi = (req, res, next) => {
+  // body contains a json object as a value which had a key of "campground"
+  // {"campground":{"title":"camp","location":"location"}}
+
+  // simple error throwing:
+  // if "campground" key is not present in the payload, throw an error:
+  // if (!req.body.campground)
+  //   throw new ExpressError("Invalid/Insufficent data", 400);
+
+  // as you can see this is tedious and quite difficult to scale.
+  // But here will use joi schema to make our job easier!
+
+  // validate() checks whether body is matching our Joi schema!
+  // result of validate(req.body) will be of following format:
+  // {
+  //   value: {
+  // original res.body
+  //          },
+  //   error: [Error [ValidationError]: "campground" is required]
+  //        {
+  //          _original: {},
+  //          details: [ [Object] ]
+  //        }
+  // }
+
+  const { error } = campgroundSchemaJoi.validate(req.body);
+
+  // notice that error.details is an array.
+
+  // if error exists, then throw expressError with all details joined by ",":
+  if (error) {
+    const msg = error.details.map(err => err.message).join(",");
+    next(new ExpressError(msg));
+  } else {
+    next();
+  }
+};
+
+// async get request for displaying all campgrounds:
+router.get(
+  "/",
+  asyncCatcher(async (req, res) => {
+    // finding all campgrounds will take time so you need await here:
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get("/new", (req, res) => {
+  const campground = {};
+  res.render("campgrounds/new", { campground });
+});
+
+// creates a dummy campground with some dummy values [to be deprecated]:
+router.get(
+  "/makecampground",
+  asyncCatcher(async (req, res) => {
+    const camp = new Campground({
+      title: "My Backyard",
+      description: "Cheap Camping",
+      image:
+        "https://cdn2.howtostartanllc.com/images/business-ideas/business-idea-images/campground.jpg"
+    });
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`);
+  })
+);
+
+// show details of individual campground using campground id:
+
+router.get(
+  "/:id",
+  asyncCatcher(async (req, res) => {
+    // finding campground with that specific id using findById(),
+    // it will always return 1 record since ids are unique:
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    // populate with reviews according to their objectid so we can show them on details page!
+    // notice that we have to write the name of the key which has value as array of objectids.
+    // in this case, "reviews" was the key which had stored the array of objectids which we want to populate with object values!
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+// go to edit page of a campground:
+router.get(
+  "/:id/edit",
+  asyncCatcher(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+// create new campground:
+
+router.post(
+  "/",
+  schemaValidatorJoi, // does the server side data validations before running the post route
+  asyncCatcher(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    // note that campground is now in a schema that we want, so we can call save on it:
+    await campground.save();
+    // now campground is a record in our database, we can access its id, so we can redirect:
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+// edit campground details into db:
+router.put(
+  "/:id/edit",
+  schemaValidatorJoi, // does the server side data validations before running the put route
+  asyncCatcher(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  asyncCatcher(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
