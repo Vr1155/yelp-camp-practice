e85a666530954bdf082e45d34fbe6bd161eb7ab3
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-11-29 17:15:10 +0530

    importing some dependencies and created a post route

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
 
-const User = require("./models/users");
-
 // setting up ejs and relative paths for views:
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// creating/connecting to a database called authDemo:
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/authDemo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Importing models:
+const User = require("./models/users");
+
+// middleware parsing body:
+app.use(express.urlencoded({ extended: true }));
+
 // routes:
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  res.send(req.body);
 });
 
 app.get("/secret", (req, res) => {
