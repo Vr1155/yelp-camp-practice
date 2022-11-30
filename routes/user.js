2b1a60abf82b7f80fa9386771fb3fd3daedc5b10
Vaibhav <vaibhav.ranshoor1155@gmail.com>
2022-12-01 00:44:57 +0530

    completed basic registeration route and logic along with some error handling.

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,6 @@ const router = express.Router();
 const User = require("../models/user");
 
 // some other dependencies updated for this route:
-const ExpressError = require("../utilities/ExpressError");
 const asyncCatcher = require("../utilities/asyncCatcher");
 
 router.get("/register", (req, res) => {
@@ -15,11 +14,31 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
-router.post("/register", (req, res) => {
-  // just for debugging:
-  res.send(req.body);
+router.post(
+  "/register",
+  asyncCatcher(async (req, res) => {
+    // just for debugging:
+    // res.send(req.body);
 
-  // const { username, password, email } = req.body;
-});
+    try {
+      //extract username, password and email:
+      const { username, password, email } = req.body;
+      // create an obj if they are according to mongoose schema:
+      const user = new User({
+        email,
+        username
+      });
+      // use static plugin method register() from "passport-local-mongoose" to register user:
+      const newUser = await User.register(user, password);
+      // display flash msg and redirect to all campgrounds:
+      req.flash("success", "Registeration Successful! Welcome to YelpCamp!");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      // if anything fails, display error flash msg and redirect back to register page:
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
 
 module.exports = router;
